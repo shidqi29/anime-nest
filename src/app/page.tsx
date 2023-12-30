228166d4dbae1bad8a3ad234d2a041ba4f2e6c8d
shidqi29 <shidqiamirulhaq@gmail.com>
2023-12-30 15:50:09 +0700

    feat: add reusable component for header animeList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,12 @@ export default async function Home() {
 
   return (
     <>
+      <section></section>
       <div className="flex w-full items-center justify-center">
         <AnimeList api={topAnime} title="Top Anime" linkHref="/top/anime" />
       </div>
       <div className="flex w-full items-center justify-center">
-        <AnimeList api={topAnime} title="Top Anime" linkHref="/top/anime" />
+        <AnimeList api={topAnime} title="Trending"/>
       </div>
     </>
   );
---
+++ src/components/AnimeList/header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import React from "react";
+
+type HeaderProps = {
+  title?: string;
+  linkHref?: string;
+};
+
+export const Header = ({ title, linkHref }: HeaderProps) => {
+  return (
+    <div className="flex items-center justify-between p-4">
+      <h2 className="text-2xl font-bold">{title}</h2>
+      {linkHref && (
+        <Link
+          href={linkHref}
+          className="transition-colors hover:text-default-500"
+        >
+          See More
+        </Link>
+      )}
+    </div>
+  );
+};

c26fcec7f208717be7a6b08c1cec70dca94b8a8d
shidqi29 <shidqiamirulhaq@gmail.com>
2024-01-16 22:51:34 +0700

    feat: add authentication with github using next-auth

---
+++ src/components/NavBar/login.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+} from "@nextui-org/react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+export const Login = () => {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      {session ? (
+        <Dropdown placement="bottom-start">
+          <DropdownTrigger>
+            <Avatar
+              as="button"
+              src={session.user?.image || ""}
+              showFallback
+              name={session.user?.name || "User"}
+              isBordered
+              className="transition-transform"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="solid">
+            <DropdownSection showDivider>
+              <DropdownItem key="profile" className="h-14 gap-2">
+                <p className="font-semibold">Signed in as</p>
+                <p className="font-semibold">{session.user?.name}</p>
+              </DropdownItem>
+            </DropdownSection>
+            <DropdownSection>
+              <DropdownItem
+                key="logout"
+                as={Link}
+                href="/api/auth/signout"
+                variant="solid"
+                color="danger"
+              >
+                Logout
+              </DropdownItem>
+            </DropdownSection>
+          </DropdownMenu>
+        </Dropdown>
+      ) : (
+        <Button
+          color="primary"
+          className="hidden md:flex"
+          as={Link}
+          href="/api/auth/signin"
+        >
+          Login
+        </Button>
+      )}
+    </>
+  );
+};
--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -1,10 +1,13 @@
+import AuthProvider from "./AuthProvider";
 import ComponentProvider from "./ComponentProvider";
 import TanstackProvider from "./TanstackProvider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <TanstackProvider>
-      <ComponentProvider>{children}</ComponentProvider>
-    </TanstackProvider>
+    <AuthProvider>
+      <TanstackProvider>
+        <ComponentProvider>{children}</ComponentProvider>
+      </TanstackProvider>
+    </AuthProvider>
   );
 }
